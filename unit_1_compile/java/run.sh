75bb39629e3bcb27016f82e0ddbd65116cdf92b6
Denis <7karavan@gmail.com>
2022-12-11 10:17:21 +0200

    add .level

--- unit_1_compile/java/run.sh
+++ unit_1_compile/java/run.sh
@@ -3,22 +3,32 @@ sd ./simple
 javac Hello.java
 java Hello
 
+cd ../
+
 echo 'run package'
-sd ../package
+sd ./package
 javac ua/olx/Hello.java
 java ua.olx.Hello
 
+cd ../
+
 echo 'run separate_packages'
-sd ../separate_packages
+sd ./separate_packages
 javac ua/olx/test/Hello.java
 java ua.olx.test.Hello
 
+cd ../
+
 echo 'run minimal_proj'
-sd ../minimal_proj
+sd ./minimal_proj
 javac src/ua/olx/test/Hello.java
 java src.ua.olx.test.Hello
 
+cd ../
+
 echo 'run med_proj'
-sd ../med_proj
+sd ./med_proj
 javac src/ua/olx/test/Hello.java
 java src.ua.olx.test.Hello
+
+cd ../../
