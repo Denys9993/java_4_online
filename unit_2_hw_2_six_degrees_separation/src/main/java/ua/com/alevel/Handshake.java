bc5a7a44361f90a87d33f83e59a9a4305b5d49af
egorfuntusov <funtushan@gmail.com>
2023-01-08 21:55:00 +0200

    add feedback

--- unit_2_hw_2_six_degrees_separation/src/main/java/ua/com/alevel/Handshake.java
+++ unit_2_hw_2_six_degrees_separation/src/main/java/ua/com/alevel/Handshake.java
@@ -1,15 +1,20 @@
 package ua.com.alevel;
 
 public class Handshake {
+
     public static String message;
     public static String newMessage = "Hallo";
     String name;
+
     Handshake(String name, String message){
         this.name=name;
-        this.message = message;}
+        this.message = message;
+    }
+
     Handshake(String name){
         this.name =name;
     }
+
     void sendMessage(Handshake handshake){
         handshake.message = newMessage;
         System.out.println("Ok get it message "+ message + " from  " + name);
--- unit_2_hw_2_six_degrees_separation/src/main/java/ua/com/alevel/Main.java
+++ unit_2_hw_2_six_degrees_separation/src/main/java/ua/com/alevel/Main.java
@@ -1,8 +1,8 @@
 package ua.com.alevel;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Handshake a1 = new Handshake("1");
         Handshake a2 = new Handshake("2", null);
         Handshake a3 = new Handshake("3", null);
@@ -10,11 +10,12 @@ public class Main {
         Handshake a5 = new Handshake("5", null);
         Handshake a6 = new Handshake("6", null);
 
-        while(a6.message == null){
+        while(a6.message == null) {
             a1.sendMessage(a2);
             a2.sendMessage(a3);
             a3.sendMessage(a4);
             a4.sendMessage(a5);
-            a5.sendMessage(a6);}
+            a5.sendMessage(a6);
+        }
     }
 }
